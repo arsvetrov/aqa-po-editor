5a0b4cee5d79e9d2df77e7aa3aa4f21b1aef12e4
arseniivetrov <arsvetrov@gmail.com>
2023-10-26 17:49:02 +0300

    allure markers added

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 
 from src.api.client.api_client import Auth
 from src.api.client.api_support import *
@@ -6,40 +7,39 @@ from test_data.payloads.user_credentials import UserData
 from test_data.responses.responses import AuthResponses
 
 
-@pytest.mark.positive
-def test_register01(remove_user):
-    """Register user with valid data"""
+class TestAuth:
+    @allure.description('Register user with valid data')
+    @pytest.mark.positive
+    def test_register01(self, remove_user):
 
-    response = Auth.register_user(UserData.register)
-    r_json = json_processing(response)
+        response = Auth.register_user(UserData.register)
+        r_json = json_processing(response)
 
-    assert response.status_code == 200
-    assert r_json == AuthResponses.success_register
+        assert response.status_code == 200
+        assert r_json == AuthResponses.success_register
 
+    @allure.description('Try to register user with existing email')
+    @pytest.mark.negative
+    def test_register02(self, registered_admin, remove_user):
 
-@pytest.mark.negative
-def test_register02(registered_admin, remove_user):
-    """Try to register user with existing email"""
+        response = Auth.register_user(UserData.register)
+        r_json = json_processing(response)
 
-    response = Auth.register_user(UserData.register)
-    r_json = json_processing(response)
+        assert response.status_code == 422
+        assert r_json.get("errors") == AuthResponses.email_exists
+        assert r_json.get("token") is None
 
-    assert response.status_code == 422
-    assert r_json.get("errors") == AuthResponses.email_exists
-    assert r_json.get("token") is None
+    @allure.description('Login user with valid data.')
+    @pytest.mark.positive
+    def test_login01(self, registered_admin, remove_user):
 
+        response = Auth.login_user(UserData.login)
+        r_json = json_processing(response)
+        token = r_json.get("token")
+        user_id = r_json.get("user")["id"]
 
-@pytest.mark.positive
-def test_login01(registered_admin, remove_user):
-    """Login user with valid data."""
-
-    response = Auth.login_user(UserData.login)
-    r_json = json_processing(response)
-    token = r_json.get("token")
-    user_id = r_json.get("user")["id"]
-
-    assert response.status_code == 201
-    assert validate_token(token) is not None
-    assert isinstance(user_id, int)
-    assert r_json.get("user")["email"] == UserData.login.get("email")
-    assert r_json.get("user")["name"] == UserData.register.get("name")
+        assert response.status_code == 201
+        assert validate_token(token) is not None
+        assert isinstance(user_id, int)
+        assert r_json.get("user")["email"] == UserData.login.get("email")
+        assert r_json.get("user")["name"] == UserData.register.get("name")
